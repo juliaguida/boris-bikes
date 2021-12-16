ac82894791f3794ee6e6a086939d92a818874703
Julia.guida <32463353+juliaguida@users.noreply.github.com>
2021-12-16 15:40:21 +0000

    added the docking to dock the bike and checked if it was docked

--- lib/dockingStation.rb
+++ lib/dockingStation.rb
@@ -1,7 +1,11 @@
 require_relative "bike"
 class DockingStation 
+  attr_reader :docked
+  def dock(bike)
+    @docked = bike
+  end
 
-  def release_bike 
+  def release_bike
     Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,24 @@
 require "dockingStation"
 describe DockingStation do
   describe "respond to" do
-  it {is_expected.to respond_to :release_bike}
+    it {is_expected.to respond_to :release_bike}
   end 
-  it 'releases working bikes' do
-    bike = subject.release_bike 
+  it 'releases a working bike' do
+    dockingStation = DockingStation.new
+    bike = dockingStation.release_bike 
     expect(bike).to be_working
   end
-end 
+  describe 'responds to dock' do 
+    it {is_expected.to respond_to(:dock).with(1).argument}
+  end
+  describe "docked bike" do 
+    it {is_expected.to respond_to(:docked)}
+  end
+  it 'docked should return bike docked using dock' do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    docking_station.dock(bike)
+    expect(docking_station.docked).to eq(bike)
+  end
+
+end
